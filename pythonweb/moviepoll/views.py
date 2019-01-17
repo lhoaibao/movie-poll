5e76935e49153315e806e408d9f952ce3e4ab1fa
lhoaibao <lehoaibao081999@gmail.com>
2019-01-17 20:00:21 +0700

    new

---
+++ pythonweb/moviepoll/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Question
+from django.urls import reverse
+# Create your views here.
+def index(request):
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'lastest_question_list': lastest_question_list}
+    return render(request, 'moviepoll/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'moviepoll/detail.html', {'question':question})
+
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'moviepoll/result.html', {'question':question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'moviepoll/detail.html', {'question':question,
+                                                         'error_message': "You didn't select a choice"
+                                                        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('moviepoll:result', args=(question.id,)))
