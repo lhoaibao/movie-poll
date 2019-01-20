d3a465e792286a732087084553129a6de6377af9
lhoaibao <lehoaibao081999@gmail.com>
2019-01-20 19:56:47 +0700

    new

--- pythonweb/pythonweb/urls.py
+++ pythonweb/pythonweb/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('moviepoll/', include('moviepoll.urls'))
+    path('', include('moviepoll.urls'))
 ]
